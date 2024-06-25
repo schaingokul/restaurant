9bc00bc360b95b267a30d3ffeeb94f5f0d830c5f
Gokul Gokul <schaingokul@gmail.com>
2024-06-25 11:35:02 +0530

    first_restaurant

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+
 import { images } from '../../constants';
 
-const SubHeading = ({tittle}) => (
-  <div style={{marginBottom:'1rem'}}>
-    <p className='p__cormorant'>{tittle}</p>
-    <img src={images.spoon} alt='spoon' className='spoon__img'/>
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon_image" className="spoon__img" />
   </div>
 );
 
